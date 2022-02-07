3c928a53595802b36a4faf4c5fa64c451b7bb88d
Randormeur <r.pirate94@gmail.com>
2022-02-07 21:20:33 +0100

    Creation  Fonction Ajout des items dans le panier

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,11 @@
+    // Recuperez l'id dans l'url de la page
+    let url = window.location.href;
+    let newUrl = new URL (url);
+    let urlSearch = newUrl.search;
+    let urlId = urlSearch.replace(/[^a-zA-Z0-9]/g, '');
+    // Fin Recuperez l'id dans l'url de la page
+
+
 /**Récuperez Produit  de l'API correspondant à l'url actuel*/
 function affichageProduit() {
     fetch ("http://localhost:3000/api/products/")
@@ -16,17 +24,11 @@ function affichageProduit() {
 // Recuperez la fiche Produit
 
 function ficheProduit(data) {
-    // Recuperez l'id dans l'url de la page
-    let url = window.location.href;
-    let newUrl = new URL (url);
-    let urlSearch = newUrl.search;
-    let urlId = urlSearch.replace(/[^a-zA-Z0-9]/g, '');
-    // Fin Recuperez l'id dans l'url de la page
 
-    let img ='';
+
     let optionColor = '';
 
-    // Modifiez la fiche image selon l'id du produit
+    // Modifiez la fiche produit selon l'id du produit
 
     for (let idProduit of data){
         if (idProduit._id == urlId){
@@ -44,14 +46,50 @@ function ficheProduit(data) {
         }
     }
 
-    // Fin -- Modifiez la fiche image selon l'id du produit
+    // Fin -- Modifiez la fiche produit selon l'id du produit
 }
+
+// Ajçuter un item dans la panier
+function addPanier (event){
+    document.getElementById("addToCart").addEventListener("click", ajoutPanier);
+    let itemDejaPresent ='';
+
+    // Fonction pour ajouter un item
+    function ajoutPanier(event){
+        let numberOfItem = Number(document.getElementById('quantity').value);
+        let itemColor = document.getElementById('colors').value;
+        let item = document.getElementById('title').innerText +" "+itemColor;
+
+        // Ajouter un item non présent dans le panier
+        if(!localStorage.getItem(item)) {
+            localStorage.setItem(`${item}`, 
+                JSON.stringify({
+                    id: `${urlId}` , quantity: `${numberOfItem}`, couleur: `${itemColor}`
+                })
+            );
+            // Fin - Ajouter un item non présent dans le panier
+        } else {
+            // Incrémenter la quantité à un item présent dans le panier
+            itemDejaPresent = JSON.parse(localStorage.getItem(item));
+            let newQuantityCanap= Number(itemDejaPresent.quantity) + numberOfItem;
+            itemDejaPresent.quantity = `${newQuantityCanap}`;
+            localStorage.setItem(`${item}`, 
+                JSON.stringify(itemDejaPresent)
+            );
+            // Fin - Incrémenter la quantité à un item présent dans le panier
+        }
+        console.log(localStorage);
+    }
+    
+
+}
+
 
 
 // Chargement de la page
 
 window.onload = function() {
     affichageProduit();
-    
+    addPanier();
 }
 
